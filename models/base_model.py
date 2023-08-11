024eff5d13b7655df736765b50ed8cac3f69067c
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-08-11 19:37:03 +0000

    class BaseModel that defines all common attributes/methods for other classes

--- models/base_model.py
+++ models/base_model.py
@@ -1,74 +1,56 @@
 #!/usr/bin/python3
-'''
-Defines BaseModel as super class for all aother classes in AirBnB Project
-'''
-import uuid
+"""module that Defines the BaseModel Class"""
+from uuid import uuid4
 from datetime import datetime
-from . import storage
+import models
+
+"""Parent class for all other classes in AirBnB Project"""
 
 
-class BaseModel:
-    '''
-    BaseModel provides all methods and attributes for
-    subclasses in the AirBnb project.
-
-    Attributes:
-        id (string): Assigned as a unique identifier for
-            each instance of BaseModel created using uuid4().
-        created_at (datetime): A Datetime object assigned to BaseModels
-            when instances are created.
-        updated_at (datetime): A datetime object assigned to each instance
-            of BaseModel at initialization, and is reset when save() is called.
-    '''
-    def __init__(self, *args, **kwargs):
-        '''Initialization method, called each time an instance is created.
-        If kwargs is not empty, an new instance is created from the key/value
-        pairs found in kwargs, otherwise a completely new object is created
-
-        Args:
-            *args: Variable length argument list.
-                It is not used when new object is initialized.
-            **kwargs: Arbitrary kewword arguments. With the following expected:
-                id, created_at, updated_at.
-        '''
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    if key in ["created_at", "updated_at"]:
-                        setattr(self, key, datetime.fromisoformat(value))
-                    else:
-                        setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
-            storage.new(self)
+class BaseModel():
+    """Parent class for all other classes in AirBnB Project
+    Methods:
+        __init__(self)
+        __str__(self)
+        __save(self)
+        __repr__(self)
+        to_dict(self)
+    """
+    def __init__(self):
+        """
+        Initialization method, called each time an instance is created.
+        Initialization attributes: uuid4, dates when class was updated/created        
+        """
+        date_format = '%Y-%m-%dT%H:%M:%S.%f'
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        '''Method to set the string representation of BaseModel object.
+        """Method to set the string representation of BaseModel object.
+        Return the class name, id and the dictionary
+        """
+        return ('[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__))
 
-        Return:
-            The string representation returned follows the following format:
-            [<class name>] (<self.id>) <self.__dict__>
-        '''
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+    def __repr__(self):
+        """returns string repr"""
+        return (self.__str__())
 
     def save(self):
-        '''Updates the public attribute `updated_at` with the current time
-        '''
-        storage.save()
+        """
+        - Updates the public attribute `updated_at` with the current time
+        -    
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        '''This method is used for serialization of object by returning the
-        dictionary of the object with added key/value pairs(__class__).
-
-        Return:
-            A dictionary containing all the keys/value of __dict__ method
-            of the instance. A key __class__ is added to this dictionary
-            with the class name of the object
-        '''
+        """
+        returns the dictionary of the BaseModel with string formats of time,
+        A dictionary containing all the keys/value of __dict__ method of the instance. 
+        A key __class__ is added to this dictionary with the class name of the object
+        """
         result = self.__dict__.copy()
-        result['created_at'] = self.created_at.isoformat()
-        result['updated_at'] = self.updated_at.isoformat()
-        result['__class__'] = self.__class__.__name__
+        result["created_at"] = self.created_at.isoformat()
+        result["updated_at"] = self.updated_at.isoformat()
+        result["__class__"] = self.__class__.__name__
         return result
